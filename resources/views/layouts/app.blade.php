dc468c8dda46dcc8b7f98aeb34aa651a3d4a9fa2
Glenn Hermans <glhermans@gmail.com>
2016-05-12 02:02:20 +0000

    hotfix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-notifications.css') }}">
     <link rel="stylesheet" href="{{ asset('js/icheck-1/skins/all.css') }}">
 
     <style>
@@ -21,9 +22,7 @@
             font-family: 'Lato';
         }
 
-        .fa-btn {
-            margin-right: 6px;
-        }
+        .fa-btn{ margin-right: 6px; }
         .team{ padding: 1px 0; }
     h6.description{
     font-weight: bold;
@@ -32,6 +31,40 @@
     border-bottom: 1px solid rgba(0, 0, 0,0.1);
     padding-bottom: 5px;
 }
+.modal-full.modal-dialog {
+    width: 99%;
+}
+
+.white-overlay {
+    background-color: rgba(255,255,255,0.85);
+}
+.dark-overlay {
+    background-color: rgba(0,0,0,0.85);
+}
+
+.modal-content {
+    border-radius: 4px !important;
+}
+
+.modal-title {
+    margin-bottom: 0;
+    font-weight: bold;
+}
+
+.modal-content {
+    border: none;
+    box-shadow: 0 5px 10px rgba(0,0,0,0.2);
+    border-radius: 0 !important;
+}
+
+.modal-content .form-horizontal .control-label{
+    text-align: left;
+}
+
+.modal-header, .modal-footer {
+    border: none;
+}
+
 .profile{
     margin-top: 25px;
 }
@@ -51,6 +84,8 @@
     position: relative;
 }
     </style>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+
 </head>
 <body id="app-layout">
     <nav class="navbar navbar-default navbar-static-top">
@@ -93,6 +128,30 @@
                 </ul>
                  <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
+<li class="dropdown dropdown-notifications">
+            <a href="#notifications-panel" class="dropdown-toggle">
+              <i data-count="2" class="glyphicon glyphicon-bell notification-icon"></i>
+            </a>
+
+            <div class="dropdown-container">
+
+              <div class="dropdown-toolbar">
+                <div class="dropdown-toolbar-actions">
+                  <a href="#">Mark all as read</a>
+                </div>
+                <h3 class="dropdown-toolbar-title">Notifications (2)</h3>
+              </div><!-- /dropdown-toolbar -->
+
+              <ul class="dropdown-menu">
+                  ...
+              </ul>
+
+              <div class="dropdown-footer text-center">
+                <a href="#">View All</a>
+              </div><!-- /dropdown-footer -->
+
+            </div><!-- /dropdown-container -->
+          </li><!-- /dropdown -->
                 <li class="dropdown">
                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -112,7 +171,6 @@
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
     <script src="{{ asset('js/icheck-1/icheck.js') }}"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
        <h1>Marrie Doi</h1>
        <h2>Technical</h2>
        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-       <p class="text-center"><button class="btn btn-success">Bel mij</button></p>
+       <p class="text-center"><button class="btn btn-success" data-toggle="modal" data-target="#myModal">Bel mij</button></p>
      </div>
     
      <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
@@ -61,7 +61,7 @@
               <h1>Christopher Di</h1>
               <h2>Technical</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              <p class="text-center"><button class="btn btn-success">Bel mij</button></p>
+              <p class="text-center"><button class="btn btn-success" data-toggle="modal" data-target="#myModal">Bel mij</button></p>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
               <div class="img-box">
@@ -70,7 +70,7 @@
               <h1>Heather H</h1>
               <h2>Technical</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              <p class="text-center"><button class="btn btn-success">Bel mij</button></p>
+              <p class="text-center"><button class="btn btn-success" data-toggle="modal" data-target="#myModal">Bel mij</button></p>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
               <div class="img-box">
@@ -91,4 +91,137 @@
     </div>
 </div>
 </div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Callback request</h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal">
+          <div class="form-group">
+           <label for="customerType" class="col-md-3 control-label">Type <strong class="text-danger">*</strong></label>
+            <div class="col-md-6">
+             <select id='customerType' name="customerType" class="form-control">
+                     <option value="" selected></option>
+                     <option value="existing">I have a account</option>
+                     <option value="new">Register</option>
+                    </select>
+                 </div>
+                </div>
+
+        <div style="display:none;" id="existing">
+
+        <div class="form-group">
+         <label for="account_number" class="col-sm-3 control-label">Customer number</label>
+          <div class="col-sm-6">
+           <input type="text" name="account_number" id="account_number" class="form-control">
+          </div>
+        </div>
+
+        <div class="form-group">
+         <label for="phone" class="col-sm-3 control-label">Phone</label>
+          <div class="col-sm-6">
+           <input type="text" name="phone" id="phone" class="form-control">
+          </div>
+        </div>
+
+        <div class="form-group">
+         <label for="email" class="col-sm-3 control-label">Email</label>
+          <div class="col-sm-6">
+           <input type="email" name="email" id="email" class="form-control">
+          </div>
+        </div>       
+       
+        </div>
+
+        <div style="display:none;" id="new">
+
+        <div class="form-group">
+         <label for="name" class="col-sm-3 control-label">Name</label>
+          <div class="col-sm-6">
+           <input type="text" name="name" id="name" class="form-control">
+          </div>
+        </div>
+
+        <div class="form-group">
+         <label for="first_name" class="col-sm-3 control-label">First name</label>
+          <div class="col-sm-6">
+           <input type="text" name="first_name" id="first_name" class="form-control">
+          </div>
+        </div>
+
+        <div class="form-group">
+         <label for="email" class="col-sm-3 control-label">Email</label>
+          <div class="col-sm-6">
+           <input type="email" name="email" id="email" class="form-control">
+          </div>
+        </div>
+
+        <div class="form-group">
+         <label for="phone" class="col-sm-3 control-label">Phone</label>
+          <div class="col-sm-6">
+           <input type="text" name="phone" id="phone" class="form-control">
+          </div>
+        </div>
+
+        <div class="form-group">
+         <label for="account_number" class="col-sm-3 control-label">Customer number</label>
+          <div class="col-sm-6">
+           <input type="text" name="account_number" id="account_number" class="form-control">
+          </div>
+        </div>
+
+      </div>
+
+        <div class="form-group">
+         <label for="email" class="col-sm-3 control-label">Product</label>
+          <div class="col-sm-6">
+           <input type="email" name="email" id="email" class="form-control">
+          </div>
+        </div>
+
+        <div class="form-group">
+         <label for="description" class="col-sm-3 control-label">Question</label>
+          <div class="col-sm-6">
+           <textarea name="description" id="description" class="form-control"></textarea>
+          </div>
+        </div>        
+     </div>
+     
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
+      </div>
+    </form>
+
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+$(document).ready(function(){
+    $('#customerType').on('change', function() {
+      if ( this.value == 'existing')
+      {
+        $("#new").hide();
+        $("#existing").show();
+      }
+      else  if ( this.value == 'new')
+      {
+          $("#existing").hide();
+        $("#new").show();
+      } 
+       else  
+      {
+        $("#existing").hide();
+        $("#new").hide();
+      }
+    });
+});
+</script>
 @endsection
