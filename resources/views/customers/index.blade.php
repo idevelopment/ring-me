346c9371092a07a2d2761bb406c1b49253ba6710
Glenn <glhermans@gmail.com>
2016-05-14 06:13:19 +0200

    Quick fix

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -61,7 +61,7 @@
             </div>
         </div>
     </div>
-
+   </div>
 
     <div class="row">
         <div class="col-md-12">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
+<div class="container">
+   <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">Open Call Requests</div>
