06d29fff81e5a4d9a6fce1f566b47840d497485e
Glenn <glhermans@gmail.com>
2016-05-14 03:32:40 +0200

    Fixed random theme issues

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,9 +50,7 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Login
-                                </button>
+                                <button type="submit" class="btn btn-primary">Login</button>
 
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
                             </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,8 @@
                 <div class="panel-body">
                  <table class="table table-striped">
                   <thead>
-                   <th>ID</th>
-                   <th>Contact</th>
+                   <th>Customer</th>
+                   <th>Agent</th>
                    <th>Type</th>
                    <th>Queue time</th>
                    <th>Status</th>
@@ -19,35 +19,15 @@
                   </thead>
                   <tbody>
                    <tr>
-                    <td>
-                     <div class="ckbox">
-                      <input type="checkbox" id="checkbox1">
-                      <label for="checkbox1"></label>
-                     </div>
-                    </td>
+                    <td><a href="javascript:;">John Porter</a></td>
                      <td><a href="javascript:;">Glenn Hermans</a></td>
                      <td>Callback</td>
                      <td><span class="text-danger">12 min</span></td>
                      <td><span class="badge bg-red">Waiting</span></td>
                      <td><a href="#"><i class="fa fa-info-circle fa-lg"></i></a></td>
                     </tr>
-
-                   <tr>
-                    <td>
-                     <div class="ckbox">
-                      <input type="checkbox" id="checkbox1">
-                      <label for="checkbox1"></label>
-                     </div>
-                    </td>
-                     <td><a href="javascript:;">Glenn Hermans</a></td>
-                     <td>Callback</td>
-                     <td><span class="text-success">2 min</span></td>
-                     <td><span class="badge bg-green">Approved</span></td>
-                     <td><a href="#"><i class="fa fa-info-circle fa-lg"></i></a></td>
-                    </tr>                    
-  
-                           </tbody>
-                        </table>
+                  </tbody>
+                 </table>
                 </div>
             </div>
         </div>
@@ -56,18 +36,18 @@
         <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Open Requests</div>
+                <div class="panel-heading">Agent history</div>
                 <div class="panel-body">
                  <ul class="notifications">
                   <li class="notification">
                    <div class="media">
                     <div class="media-left">
                      <div class="media-object">
-            <img data-src="holder.js/50x50?bg=cccccc" class="img-circle" alt="Name">
+            <img src="{{ asset('img/user-icon.png') }}"  width="50" height="50" class="img-circle" alt="Name">
           </div>
         </div>
         <div class="media-body">
-          <strong class="notification-title"><a href="#">Dave Lister</a> ended call with <a href="#">iDevelopment</a></strong>
+          <strong class="notification-title"><a href="#">Glenn Hermans</a> ended call with <a href="#">iDevelopment</a></strong>
           <p class="notification-desc">I totally don't wanna do it. Rimmer can do it.</p>
 
           <div class="notification-meta">
@@ -81,7 +61,7 @@
       <div class="media">
         <div class="media-left">
           <div class="media-object">
-            <img data-src="holder.js/50x50?bg=cccccc" class="img-circle" alt="Name">
+            <img src="{{ asset('img/user-icon.png') }}"  width="50" height="50" class="img-circle" alt="Name">
           </div>
         </div>
         <div class="media-body">
@@ -101,7 +81,7 @@
       <div class="media">
         <div class="media-left">
           <div class="media-object">
-            <img data-src="holder.js/50x50?bg=cccccc" class="img-circle" alt="Name">
+          <img src="{{ asset('img/user-icon.png') }}"  width="50" height="50" class="img-circle" alt="Name">
           </div>
         </div>
         <div class="media-body">
