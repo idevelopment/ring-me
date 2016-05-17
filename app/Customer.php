d547546c436b37f365f04990432adb1f9fecebb0
Tjoosten <Tjoosten3@gmail.com>
2016-05-17 14:45:30 +0200

    Add fillable option for mass-assign

--- app/Customer.php
+++ app/Customer.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     protected $table = 'customers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'company', 'fname', 'name', 'address',
+        'zipcode', 'city', 'country', 'phone',
+        'mobile', 'email', 'vat'
+    ];
 }
