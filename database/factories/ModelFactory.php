6e515d52bf9ca63f7f323341e0c6464947de3811
Tjoosten <Tjoosten3@gmail.com>
2016-05-17 13:10:52 +0200

    Fix user factory bug

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -10,13 +10,14 @@
 | database. Just tell the factory how a default model should look.
 |
 */
-
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
+        'name'           => $faker->name,
+        'fname'          => $faker->firstName,
+        'email'          => $faker->safeEmail,
+        'password'       => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+        'biography'      => $faker->text(150)
     ];
 });
 
