5f9218644ea34bdc3550f7610353847eb77a6336
Tjoosten <Tjoosten3@gmail.com>
2016-05-17 12:44:05 +0200

    Update #19 -> Add departments, model, migration , factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Departments::class, function (Faker\Generator $faker) {
+    return [
+        'name'    => $faker->name,
+        'manager' => $faker->numberBetween(0, 120)
+    ];
+});
+
