6e149076e70f4fb97c621f21d4e03eaf89499a37
Tjoosten <Tjoosten3@gmail.com>
2016-05-18 08:19:54 +0200

    Fix #27 -> reformat the view files.

--- resources/views/customers/register.blade.php
+++ resources/views/customers/register.blade.php
@@ -1,103 +1,112 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <ul class="breadcrumb">
-        <li><a href="#">Home</a></li>
-        <li><a href="{{ url('customers') }}">Customers</a></li>
-        <li class="active">{{trans('customers.register')}}</li>
-    </ul>
+    <div class="container">
+        <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li><a href="{{ url('customers') }}">Customers</a></li>
+            <li class="active">{{trans('customers.register')}}</li>
+        </ul>
 
-   <form action="{{ url('customers') }}"  method="post" class="form-horizontal">
-       {!! csrf_field() !!}
+        <form action="{{ url('customers') }}" method="post" class="form-horizontal">
+            {!! csrf_field() !!}
 
-       <div class="form-group form-sep">
-        <label for="company" class="col-md-3 control-label">{{trans('customers.company')}} <span class="text-danger">*</span></label>
+            <div class="form-group form-sep">
+                <label for="company" class="col-md-3 control-label">{{trans('customers.company')}} <span
+                            class="text-danger">*</span></label>
 
-         <div class="col-md-8">
-           <input type="text" name="company" id="company" class="form-control">
-         </div>
-        </div>
+                <div class="col-md-8">
+                    <input type="text" name="company" id="company" class="form-control">
+                </div>
+            </div>
 
-       <div class="form-group form-sep">
-        <label for="vat" class="col-md-3 control-label">{{trans('customers.vat')}} <span class="text-danger">*</span></label>
-         <div class="col-md-8">
-           <input type="text" name="vat" id="vat" class="form-control">
-         </div>
-        </div>        
-        
-        <div class="form-group formSep">
-         <label for="name" class="col-md-3 control-label">{{trans('customers.name')}} <span class="text-danger">*</span></label>
-          <div class="col-md-8">
-            <input type="text" name="name" id="name" class="form-control">
-          </div>
-        </div>
+            <div class="form-group form-sep">
+                <label for="vat" class="col-md-3 control-label">{{trans('customers.vat')}} <span
+                            class="text-danger">*</span></label>
+                <div class="col-md-8">
+                    <input type="text" name="vat" id="vat" class="form-control">
+                </div>
+            </div>
 
-        <div class="form-group formSep">
-         <label for="firstname" class="col-md-3 control-label">{{trans('customers.first_name')}} <span class="text-danger">*</span></label>
-          <div class="col-md-8">
-            <input type="text" name="fname" id="firstname" class="form-control">
-          </div>
-        </div>
+            <div class="form-group formSep">
+                <label for="name" class="col-md-3 control-label">{{trans('customers.name')}} <span
+                            class="text-danger">*</span></label>
+                <div class="col-md-8">
+                    <input type="text" name="name" id="name" class="form-control">
+                </div>
+            </div>
 
-        <div class="form-group formSep">
-          <label for="address" class="col-md-3 control-label">{{trans('customers.address')}} <span class="text-danger">*</span></label>
-           <div class="col-md-8">
-            <input type="text" name="address" id="address" class="form-control">
-           </div>
-        </div> 
+            <div class="form-group formSep">
+                <label for="firstname" class="col-md-3 control-label">{{trans('customers.first_name')}} <span
+                            class="text-danger">*</span></label>
+                <div class="col-md-8">
+                    <input type="text" name="fname" id="firstname" class="form-control">
+                </div>
+            </div>
 
-        <div class="form-group formSep">
-          <label for="zipcode" class="col-md-3 control-label">{{trans('customers.zipcode')}} <span class="text-danger">*</span></label>
-           <div class="col-md-8">
-            <input type="text" name="zipcode" id="zipcode" class="form-control">
-           </div>
-        </div>
+            <div class="form-group formSep">
+                <label for="address" class="col-md-3 control-label">{{trans('customers.address')}} <span
+                            class="text-danger">*</span></label>
+                <div class="col-md-8">
+                    <input type="text" name="address" id="address" class="form-control">
+                </div>
+            </div>
 
-        <div class="form-group formSep">
-          <label for="city" class="col-md-3 control-label">{{trans('customers.city')}} <span class="text-danger">*</span></label>
-           <div class="col-md-8">
-            <input type="text" name="city" id="city" class="form-control">
-           </div>
-        </div>
+            <div class="form-group formSep">
+                <label for="zipcode" class="col-md-3 control-label">{{trans('customers.zipcode')}} <span
+                            class="text-danger">*</span></label>
+                <div class="col-md-8">
+                    <input type="text" name="zipcode" id="zipcode" class="form-control">
+                </div>
+            </div>
 
-        <div class="form-group formSep">
-          <label for="city" class="col-md-3 control-label">{{trans('customers.country')}} <span class="text-danger">*</span></label>
-           <div class="col-md-8">
-            <select name="country" id="country" class="form-control">
-             <option value="" selected=""></option>
-                <option value="Belgium">Belgium</option>
-                <option value="Netherlands">Netherlands</option>
-            </select>
-           </div>
-        </div> 
+            <div class="form-group formSep">
+                <label for="city" class="col-md-3 control-label">{{trans('customers.city')}} <span
+                            class="text-danger">*</span></label>
+                <div class="col-md-8">
+                    <input type="text" name="city" id="city" class="form-control">
+                </div>
+            </div>
 
-       <div class="form-group formSep">
-         <label for="name" class="col-md-3 control-label">{{trans('customers.email')}} <span class="text-danger">*</span></label>
-          <div class="col-md-8">
-            <input type="text" name="email" id="email" class="form-control">
-          </div>
-        </div>         
+            <div class="form-group formSep">
+                <label for="city" class="col-md-3 control-label">{{trans('customers.country')}} <span
+                            class="text-danger">*</span></label>
+                <div class="col-md-8">
+                    <select name="country" id="country" class="form-control">
+                        <option value="" selected=""></option>
+                        <option value="Belgium">Belgium</option>
+                        <option value="Netherlands">Netherlands</option>
+                    </select>
+                </div>
+            </div>
 
-        <div class="form-group formSep">
-          <label for="phone" class="col-md-3 control-label">{{trans('customers.phone')}} <span class="text-danger">*</span></label>
-           <div class="col-md-8">
-            <input type="text" name="phone" id="phone" class="form-control">
-           </div>
-        </div>
+            <div class="form-group formSep">
+                <label for="name" class="col-md-3 control-label">{{trans('customers.email')}} <span class="text-danger">*</span></label>
+                <div class="col-md-8">
+                    <input type="text" name="email" id="email" class="form-control">
+                </div>
+            </div>
 
-       <div class="form-group formSep">
-         <label for="name" class="col-md-3 control-label">{{trans('customers.mobile')}}</label>
-          <div class="col-md-8">
-            <input type="text" name="mobile" id="mobile" class="form-control">
-          </div>
-        </div> 
-       <div class="form-group">
-         <label for="name" class="col-md-3 control-label">&nbsp;</label>
-          <div class="col-md-8">
-            <button type="submit" class="btn btn-primary">{{ trans('app.save')}}</button>
-          </div>
-        </div>               
-  </form>
-</div>
+            <div class="form-group formSep">
+                <label for="phone" class="col-md-3 control-label">{{trans('customers.phone')}} <span
+                            class="text-danger">*</span></label>
+                <div class="col-md-8">
+                    <input type="text" name="phone" id="phone" class="form-control">
+                </div>
+            </div>
+
+            <div class="form-group formSep">
+                <label for="name" class="col-md-3 control-label">{{trans('customers.mobile')}}</label>
+                <div class="col-md-8">
+                    <input type="text" name="mobile" id="mobile" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="name" class="col-md-3 control-label">&nbsp;</label>
+                <div class="col-md-8">
+                    <button type="submit" class="btn btn-primary">{{ trans('app.save')}}</button>
+                </div>
+            </div>
+        </form>
+    </div>
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,104 +1,112 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-   <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">Open Call Requests</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Open Call Requests</div>
 
-                <div class="panel-body">
-                 <table class="table table-striped">
-                  <thead>
-                   <th>Customer</th>
-                   <th>Agent</th>
-                   <th>Type</th>
-                   <th>Queue time</th>
-                   <th>Status</th>
-                   <th class="col-md-1"></th>
-                  </thead>
-                  <tbody>
-                   <tr>
-                    <td><a href="javascript:;">John Porter</a></td>
-                     <td><a href="javascript:;">Glenn Hermans</a></td>
-                     <td>Administration</td>
-                     <td><span class="text-danger">12 min</span></td>
-                     <td><span class="badge bg-red">Waiting</span></td>
-                     <td><a href="#" data-toggle="tooltip" data-placement="bottom" title="{{ trans('app.details')}}"><i class="fa fa-info-circle fa-lg"></i></a></td>
-                    </tr>
-                  </tbody>
-                 </table>
+                    <div class="panel-body">
+                        <table class="table table-striped">
+                            <thead>
+                            <th>Customer</th>
+                            <th>Agent</th>
+                            <th>Type</th>
+                            <th>Queue time</th>
+                            <th>Status</th>
+                            <th class="col-md-1"></th>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td><a href="javascript:;">John Porter</a></td>
+                                <td><a href="javascript:;">Glenn Hermans</a></td>
+                                <td>Administration</td>
+                                <td><span class="text-danger">12 min</span></td>
+                                <td><span class="badge bg-red">Waiting</span></td>
+                                <td><a href="#" data-toggle="tooltip" data-placement="bottom"
+                                       title="{{ trans('app.details')}}"><i class="fa fa-info-circle fa-lg"></i></a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
         <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">Agent history</div>
-                <div class="panel-body">
-                 <ul class="notifications">
-                  <li class="notification">
-                   <div class="media">
-                    <div class="media-left">
-                     <div class="media-object">
-            <img src="{{ asset('img/user-icon.png') }}"  width="50" height="50" class="img-circle" alt="Name">
-          </div>
-        </div>
-        <div class="media-body">
-          <strong class="notification-title"><a href="#">Glenn Hermans</a> ended call with <a href="#">iDevelopment</a></strong>
-          <p class="notification-desc">I totally don't wanna do it. Rimmer can do it.</p>
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Agent history</div>
+                    <div class="panel-body">
+                        <ul class="notifications">
+                            <li class="notification">
+                                <div class="media">
+                                    <div class="media-left">
+                                        <div class="media-object">
+                                            <img src="{{ asset('img/user-icon.png') }}" width="50" height="50"
+                                                 class="img-circle" alt="Name">
+                                        </div>
+                                    </div>
+                                    <div class="media-body">
+                                        <strong class="notification-title"><a href="#">Glenn Hermans</a> ended call with
+                                            <a href="#">iDevelopment</a></strong>
+                                        <p class="notification-desc">I totally don't wanna do it. Rimmer can do it.</p>
 
-          <div class="notification-meta">
-            <small class="timestamp">27. 11. 2015, 15:00</small>
-          </div>
-        </div>
-      </div>
-  </li>
+                                        <div class="notification-meta">
+                                            <small class="timestamp">27. 11. 2015, 15:00</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
 
-  <li class="notification">
-      <div class="media">
-        <div class="media-left">
-          <div class="media-object">
-            <img src="{{ asset('img/user-icon.png') }}"  width="50" height="50" class="img-circle" alt="Name">
-          </div>
-        </div>
-        <div class="media-body">
-          <strong class="notification-title"><a href="#">Nikola Tesla</a> resolved <a href="#">T-14 - Awesome stuff</a></strong>
+                            <li class="notification">
+                                <div class="media">
+                                    <div class="media-left">
+                                        <div class="media-object">
+                                            <img src="{{ asset('img/user-icon.png') }}" width="50" height="50"
+                                                 class="img-circle" alt="Name">
+                                        </div>
+                                    </div>
+                                    <div class="media-body">
+                                        <strong class="notification-title"><a href="#">Nikola Tesla</a> resolved <a
+                                                    href="#">T-14 - Awesome stuff</a></strong>
 
-          <p class="notification-desc">Resolution: Fixed, Work log: 4h</p>
+                                        <p class="notification-desc">Resolution: Fixed, Work log: 4h</p>
 
-          <div class="notification-meta">
-            <small class="timestamp">27. 10. 2015, 08:00</small>
-          </div>
+                                        <div class="notification-meta">
+                                            <small class="timestamp">27. 10. 2015, 08:00</small>
+                                        </div>
 
-        </div>
-      </div>
-  </li>
+                                    </div>
+                                </div>
+                            </li>
 
-  <li class="notification">
-      <div class="media">
-        <div class="media-left">
-          <div class="media-object">
-          <img src="{{ asset('img/user-icon.png') }}"  width="50" height="50" class="img-circle" alt="Name">
-          </div>
-        </div>
-        <div class="media-body">
-          <strong class="notification-title"><a href="#">James Bond</a> resolved <a href="#">B-007 - Desolve Spectre organization</a></strong>
+                            <li class="notification">
+                                <div class="media">
+                                    <div class="media-left">
+                                        <div class="media-object">
+                                            <img src="{{ asset('img/user-icon.png') }}" width="50" height="50"
+                                                 class="img-circle" alt="Name">
+                                        </div>
+                                    </div>
+                                    <div class="media-body">
+                                        <strong class="notification-title"><a href="#">James Bond</a> resolved <a
+                                                    href="#">B-007 - Desolve Spectre organization</a></strong>
 
-          <div class="notification-meta">
-            <small class="timestamp">1. 9. 2015, 08:00</small>
-          </div>
+                                        <div class="notification-meta">
+                                            <small class="timestamp">1. 9. 2015, 08:00</small>
+                                        </div>
 
-        </div>
-      </div>
-  </li>
-  </ul>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-</div>
+    </div>
 @endsection
