1ef5698224fe292ea9179f8a65de2ffa56e55cc2
Tjoosten <Tjoosten3@gmail.com>
2016-05-19 17:48:27 +0200

    Add docblocks

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -6,13 +6,28 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+/**
+ * Class CallbackController
+ * @package App\Http\Controllers
+ */
 class CallbackController extends Controller
 {
+    /**
+     * Display all the callbacks.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
     	return view('callbacks/list');
     }
 
+    /**
+     * Show update form for a callback.
+     * 
+     * @param  int $id the callback id in the database.
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function edit($id)
     {
     	return view('callbacks/details');
