fdcee029455f23cff93b67ead4d5dcf9a26c5ec9
Glenn Hermans <glhermans@gmail.com>
2016-05-23 15:55:30 +0000

    Updated lang vars for profile settings

---
+++ resources/lang/en/profile.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'Profile'  => 'Profile',
+    'settings' => 'Profile settings',
+    'security' => 'Security',
+    'api'    => 'API documentation',
+    'bug'      => 'Bugs',    
+
+    // API tokens
+    'apiTokens'    => 'API tokens',
+    'newApi'    => 'New API token',    
+    'service'     => 'Service',
+    'serviceName'     => 'Service name',
+    'addToken'    => 'Add token',
+
+
+    // Security
+    'newPass'    => 'New password',
+    'confirmPass' => 'Password confirmation',
+    ];
