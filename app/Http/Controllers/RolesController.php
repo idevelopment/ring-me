096f1abda79fc5478163e1c5c9d78b1a2443b23d
Glenn Hermans <glhermans@gmail.com>
2016-05-24 18:17:04 +0000

    Fixed incorrect roles model

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use Silber\Bouncer\Database\Constraints\Roles;
+
+use App\Roles as Roles;
 
 class RolesController extends Controller
 {
