e5c178141f0d1cc5c5693dccde43ac0114b6c09e
Tjoosten <Tjoosten3@gmail.com>
2016-05-24 18:19:58 +0200

    Add missing docblocks to the role controller

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -9,14 +9,24 @@ use App\Roles;
 
 class RolesController extends Controller
 {
+    /**
+     * Get the role overview.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
     	$data['roles'] = Roles::all();
     	return view('roles.index', $data);
     }
 
+    /**
+     * Create a role in the database.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function store()
     {
-    	return view('roles.index');
+
     }    
 }
