faca46f7aff26d62047e3690cdcf003686b4caf2
Glenn Hermans <glhermans@gmail.com>
2016-05-24 22:40:58 +0200

    Fixed merge bugs (#78)
    * Fixed merge bugs
    
    * Test

---
+++ app/Http/Controllers/RolesController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+use App\Roles as Roles;
+
+class RolesController extends Controller
+{
+    /**
+     * Get the role overview.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+    	$data['roles'] = Roles::all();
+    	return view('roles.index', $data);
+    }
+
+    /**
+     * Search for a specific role.
+     *
+     * @param  Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function search(Request $request)
+    {
+        $term = $request->get('term');
+        $data['roles'] = Roles::where('name', 'LIKE', "$term")->get();
+        return view('roles.index', $data);
+    }
+
+    /**
+     * Get a specific role. and display it.
+     *
+     * @param  int $id the id off the role in the database.
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $data['query'] = Roles::find($id);
+        return view('roles.specific', $data);
+    }
+
+
+    /**
+     * Store a new role in the database.
+     * 
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store()
+    {
+        return redirect(302)->back();
+    }
+
+    /**
+     * Display the form for creating a new role.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('roles.create');
+    }
+
+    /**
+     * Get the edit form for the selected role.
+     * @param  int $id The role id in the database.
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $data['query'] = Roles::find($id);
+        return view('roles.edit', $data);
+    }
+
+    /**
+     * Update a role in the database.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update()
+    {
+        return redirect()->back(302);
+    }
+
+    /**
+     * Destroy a role out off the database.
+     *
+     * @param  int $id The id in the database for the role.
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy($id)
+    {
+        Roles::destroy($id);
+        session()->flash('message', 'Role deleted');
+        return redirect()->back(302);
+    }
+}
+
---
+++ app/Http/routes.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Authencation routes.
+Route::auth();
+
+// Department routes.
+Route::resource('departments', 'DepartmentController');
+Route::post('departments/search', 'DepartmentController@search')->name('departments.search'); 
+
+// API token routes.
+Route::post('api/token/create', 'ApiKeyController@makeKey')->name('token.create');
+
+// Home routes
+Route::get('/home', 'HomeController@index');
+
+// Callback routes
+Route::get('/callbacks', 'CallbackController@index')->name('callbacks.index');
+Route::get('/callbacks/register', 'CallbackController@create')->name('callbacks.register');
+Route::get('/callbacks/display/{id}', 'CallbackController@edit')->name('callbacks.display');
+Route::get('/callbacks/delete/{id}', 'CallbackController@destroy')->name('callbacks.destroy');
+Route::post('/callbacks', 'CallbackController@store')->name('callbacks.store');
+
+// Customers routes
+Route::get('/customers', 'CustomersController@index')->name('customers.index');
+Route::get('/customers/register', 'CustomersController@register')->name('customers.register');
+Route::get('/customers/display/{id}', 'CustomersController@edit')->name('customers.display');
+Route::post('/customers', 'CustomersController@store')->name('customers.store');
+
+// Status routes
+Route::get('/status/available', 'StaffController@setAvailable')->name('status.available');
+Route::get('/status/unavailable', 'StaffController@setUnavailable')->name('status.unavailable');
+
+// Profile routes
+Route::get('/profile', 'StaffController@profile')->name('staff.index');
+Route::post('/profile/update/password', 'ProfileController@updateSecurity')->name('profile.update.security');
+Route::post('/profile/update/profile', 'ProfileController@updateProfile')->name('profile.update.profile');
+
+// Staff routes
+Route::get('/staff', 'StaffController@index')->name('profile');
+Route::get('/staff/edit/{id}', 'StaffController@edit')->name('staff.edit');
+Route::post('/staff/edit/{id}', 'StaffController@update')->name('staff.update');
+Route::get('/staff/create', 'StaffController@create')->name('staff.create');
+Route::post('/staff/create', 'StaffController@store')->name('staff.store');
+Route::get('/staff/delete/{id}', 'StaffController@destroy')->name('staff.delete');
+
+Route::get('/staff/getdepartments', 'DepartmentController@get_departments')->name('staff.getdepartments');
+Route::get('/staff/getroles', 'StaffController@get_roles')->name('staff.getroles');
+
+// Settings routes
+Route::get('/settings', 'SettingsController@index')->name('settings');
+
+// Roles routes
+Route::get('/roles', 'RolesController@index')->name('roles');
+
+Route::post('/roles/create', 'RolesController@store')->name('roles.store');
+Route::post('/roles', 'RolesController@store')->name('roles.store');
+
+Route::get('/roles/edit/{id}', 'RolesController@edit')->name('roles.edit');
+Route::post('/roles/edit/{id}', 'RolesController@update')->name('roles.update');
