bf98cc878ecaea14f4ae15221d66ebcd138c5f95
Tim Joosten <Tjoosten3@gmail.com>
2016-09-19 04:52:31 +0200

    UPDATE #107 | Add products categories relation

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,22 +8,27 @@ use App\Http\Requests;
 
 class ProductsController extends Controller
 {
-  /**
-   * ProductsController constructor.
-   */
-  public function __construct()
-  {
-      $this->middleware('auth');
-      $this->middleware('lang');
-  }
+    /**
+     * ProductsController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('lang');
+    }
 
-  public function index()
-  {
-    return view('products.index');
-  }
+    public function index()
+    {
+        return view('products.index');
+    }
 
-  public function categories()
-  {
-    return view('products.categories');
-  }
+    public function categories()
+    {
+        return view('products.categories');
+    }
+
+    public function store()
+    {
+
+    }
 }
--- app/Products.php
+++ app/Products.php
@@ -23,4 +23,15 @@ class Products extends Model
      * @var array
      */
     protected $fillable = ['name', 'category'];
+
+
+    /**
+     * Product category relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\ProductsCategory', 'id', 'category');
+    }
 }
