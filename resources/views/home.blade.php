bfbdbe506183295baf863fafce8e7365b0c296d1
ghermans <glhermans@gmail.com>
2019-01-30 15:16:43 +0100

    authentication

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.frontend')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    Hello world
+                    You are logged in!
                 </div>
             </div>
         </div>
