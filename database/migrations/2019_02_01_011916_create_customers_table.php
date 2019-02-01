e8af8f94aa00748ba0b83e341fa8ebcddf370c80
ghermans <glhermans@gmail.com>
2019-02-01 02:22:00 +0100

    Added customers migration

---
+++ database/migrations/2019_02_01_011916_create_customers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('company');
+            $table->string('name');            
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('address');
+            $table->string('zipcode');
+            $table->string('city');
+            $table->string('country');
+            $table->string('phone');
+            $table->string('mobile');
+            $table->string('email')->unique();
+            $table->string('vat');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
